b0679d688d84d078943dad3490c90db7e94afa1f
Shubham Acharya <sacharya@cdac.in>
2026-06-24 19:46:32 +0530

    Added force deletion of ECR repo

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -27,6 +27,7 @@ output "aws_ecr_repository_arn" {
 
 resource "aws_ecr_repository" "private_repo" {
   name = var.project_name
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
